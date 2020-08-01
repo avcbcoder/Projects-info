6d8d7f7e29544182058679a805e6777c3b766715
Ankit verma <ankitvermamat@gmail.com>
2020-08-01 15:54:27 +0530

    added project collab

--- swiper/after-swiper.js
+++ swiper/after-swiper.js
@@ -1,24 +1,49 @@
-var swiper = new Swiper('.swiper-container', {
-    // effect: 'coverflow',
-    spaceBetween: 50,
-    initialSlide:1,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    // coverflowEffect: {
-    //     rotate: 0,// set to 0 to stop rotation
-    //     stretch: 0,
-    //     depth: 0,
-    //     modifier: 1,
-    //     slideShadows: true,
-    // },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable:true
-      },
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction:false
-      },
+var swiperTappy = new Swiper(".tappy-swipe", {
+  // effect: 'coverflow',
+  spaceBetween: 50,
+  initialSlide: 1,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  // coverflowEffect: {
+  //     rotate: 0,// set to 0 to stop rotation
+  //     stretch: 0,
+  //     depth: 0,
+  //     modifier: 1,
+  //     slideShadows: true,
+  // },
+  pagination: {
+    el: ".tappy-dots",
+    type: "bullets",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiperCollab = new Swiper(".collab-swipe", {
+  // effect: 'coverflow',
+  spaceBetween: 50,
+  initialSlide: 1,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  // coverflowEffect: {
+  //     rotate: 0,// set to 0 to stop rotation
+  //     stretch: 0,
+  //     depth: 0,
+  //     modifier: 1,
+  //     slideShadows: true,
+  // },
+  pagination: {
+    el: ".collab-dots",
+    type: "bullets",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
 });
